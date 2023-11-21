aaa3f6223fe406832e081895427db78e33de7dd0
HerbalTea312 <65543409+HerbalTea312@users.noreply.github.com>
2023-11-21 22:50:06 +0400

    fixed eng parts of day

--- src/components/Mobile/DayDetails/Details.jsx
+++ src/components/Mobile/DayDetails/Details.jsx
@@ -1,11 +1,22 @@
 import Item from '../Item/Item';
 import React from 'react';
 
+const wordMapping = {
+    morning: 'утро',
+    day: 'день',
+    evening: 'вечер',
+    night: 'ночь'
+  };
+  
+  function getDayPart(word) {
+    return wordMapping[word];
+  }
+
 const Details = function ({details}) {
     return (
         <div>
             {details.times.map(time =>
-                <Item details={time} time={time.time} key={time.time}/>
+                <Item details={time} time={getDayPart(time.time)} key={time.time}/>
             )}
         </div>
     );
