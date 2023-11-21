cef6679c4c9a5c8fe98cecb5cf9669873b7e55a0
HerbalTea312 <65543409+HerbalTea312@users.noreply.github.com>
2023-11-21 22:51:24 +0400

    changed getPartDay function

--- src/components/Mobile/DayDetails/Details.jsx
+++ src/components/Mobile/DayDetails/Details.jsx
@@ -1,22 +1,21 @@
 import Item from '../Item/Item';
 import React from 'react';
 
-const wordMapping = {
-    morning: 'утро',
-    day: 'день',
-    evening: 'вечер',
-    night: 'ночь'
-  };
-  
-  function getDayPart(word) {
-    return wordMapping[word];
-  }
+function getDayPart(word) {
+    const dayParts = {
+        morning: 'утро',
+        day: 'день',
+        evening: 'вечер',
+        night: 'ночь'
+    };
+    return dayParts[word];
+}
 
-const Details = function ({details}) {
+const Details = function ({ details }) {
     return (
         <div>
             {details.times.map(time =>
-                <Item details={time} time={getDayPart(time.time)} key={time.time}/>
+                <Item details={time} time={getDayPart(time.time)} key={time.time} />
             )}
         </div>
     );
